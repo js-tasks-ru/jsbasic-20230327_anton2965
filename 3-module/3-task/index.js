dadb6453d60981c186c43d5ce46ff573520ff41b
mad <mad@infodev.ru>
2023-04-12 22:17:55 +1000

    Решил задачи по Функциям 1, 2, 3, 4, 5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let reducer = (acc, value) => {
+    if (str.indexOf(value) === 0) {
+      acc.push(value);
+    } else {
+      acc.push(value.charAt(0).toUpperCase() + value.slice(1));
+    };
+    return acc;
+  };
+  return str.split('-').reduce(reducer, []).join('')
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,3 @@
 function showSalary(users, age) {
-  // ваш код...
+  return users.filter(value => value.age <= age).map(value => `${value.name}, ${value.balance}`).join('\n');
 }
